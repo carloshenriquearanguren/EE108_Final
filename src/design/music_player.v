6775fd0bb11e719b2c11e66ed4a348c93eecd20b
carloshenriquearanguren <cha4@stanford.edu>
2025-12-03 11:21:16 -0800

    Refactor music_player module
    Updated comments for clarity and removed unnecessary lines.

--- src/design/music_player.v
+++ src/design/music_player.v
@@ -1,9 +1,7 @@
 //
 //  music_player module
 //
-//  This music_player module connects up the MCU, song_reader, note_player,
-//  beat_generator, and codec_conditioner. It provides an output that indicates
-//  a new sample (new_sample_generated) which will be used in lab 5.
+//  Connects MCU, song_reader, note_player, beat_generator, echo, and codec_conditioner.
 //
 
 module music_player(
@@ -26,23 +24,18 @@ module music_player(
     output wire [15:0] sample_out
 );
     // The BEAT_COUNT is parameterized so you can reduce this in simulation.
-    // If you reduce this to 100 your simulation will be 10x faster.
     parameter BEAT_COUNT = 1000;
-
 
 //
 //  ****************************************************************************
 //      Master Control Unit
 //  ****************************************************************************
-//   The reset_player output from the MCU is run only to the song_reader because
-//   we don't need to reset any state in the note_player. If we do it may make
-//   a pop when it resets the output sample.
 //
- 
     wire play;
     wire reset_player;
     wire [1:0] current_song;
     wire song_done;
+
     mcu mcu(
         .clk(clk),
         .reset(reset),
@@ -63,6 +56,7 @@ module music_player(
     wire [5:0] duration_for_note;
     wire new_note;
     wire note_done;
+
     song_reader song_reader(
         .clk(clk),
         .reset(reset | reset_player),
@@ -75,20 +69,37 @@ module music_player(
         .note_done(note_done)
     );
 
-//   
+//
 //  ****************************************************************************
-//      Note Player
+//      Note Player (now generating chords)
 //  ****************************************************************************
-//  
+//
     wire beat;
     wire generate_next_sample, generate_next_sample0;
     wire [15:0] note_sample, note_sample0;
     wire note_sample_ready, note_sample_ready0;
 
-    // These pipeline registers were added to decrease the length of the critical path!
-    dffr pipeline_ff_gen_next_sample (.clk(clk), .r(reset), .d(generate_next_sample0), .q(generate_next_sample));
-    dffr #(.WIDTH(16)) pipeline_ff_note_sample (.clk(clk), .r(reset), .d(note_sample0), .q(note_sample));
-    dffr pipeline_ff_new_sample_ready (.clk(clk), .r(reset), .d(note_sample_ready0), .q(note_sample_ready));
+    // Pipeline registers to reduce critical path
+    dffr pipeline_ff_gen_next_sample (
+        .clk(clk),
+        .r(reset),
+        .d(generate_next_sample0),
+        .q(generate_next_sample)
+    );
+
+    dffr #(.WIDTH(16)) pipeline_ff_note_sample (
+        .clk(clk),
+        .r(reset),
+        .d(note_sample0),
+        .q(note_sample)
+    );
+
+    dffr pipeline_ff_new_sample_ready (
+        .clk(clk),
+        .r(reset),
+        .d(note_sample_ready0),
+        .q(note_sample_ready)
+    );
 
     note_player note_player(
         .clk(clk),
@@ -103,15 +114,12 @@ module music_player(
         .sample_out(note_sample0),
         .new_sample_ready(note_sample_ready0)
     );
-      
-//   
+
+//
 //  ****************************************************************************
 //      Beat Generator
 //  ****************************************************************************
-//  By default this will divide the generate_next_sample signal (48kHz from the
-//  codec's new_frame input) down by 1000, to 48Hz. If you change the BEAT_COUNT
-//  parameter when instantiating this you can change it for simulation.
-//  
+//
     beat_generator #(.WIDTH(10), .STOP(BEAT_COUNT)) beat_generator(
         .clk(clk),
         .reset(reset),
@@ -119,23 +127,69 @@ module music_player(
         .beat(beat)
     );
 
-//  
+//
+//  ****************************************************************************
+//      Echo (post-effect on mixed audio)
+//  ****************************************************************************
+//
+    wire signed [15:0] echo_in;
+    wire signed [15:0] echo_out;
+
+    // Cast pipeline note sample to signed for echo
+    assign echo_in = note_sample;
+
+    // For now: echo always enabled, fixed delay and attenuation.
+    // Later you can replace these with switches/buttons or MCU outputs.
+    wire        echo_enable    = 1'b1;
+    wire [14:0] echo_delay     = 15'd4800; // ~100 ms at 48 kHz if buffer large
+    wire [2:0]  echo_atten_sh  = 3'd1;     // /8 for wet signal
+
+    echo #(
+        .SAMPLE_WIDTH(16),
+        .ADDR_BITS(15)          // 2^15 = 32768 samples
+    ) echo_inst (
+        .clk(clk),
+        .reset(reset),
+        .new_sample_ready(note_sample_ready),
+        .in_sample(echo_in),
+        .echo_enable(echo_enable),
+        .delay_samples(echo_delay),
+        .atten_shift(echo_atten_sh),
+        .out_sample(echo_out)
+    );
+
+//
 //  ****************************************************************************
 //      Codec Conditioner
 //  ****************************************************************************
-//  
+//
     wire new_sample_generated0;
-    wire [15:0] sample_out0; 
+    wire [15:0] sample_out0;
 
-    dffr pipeline_ff_nsg (.clk(clk), .r(reset), .d(new_sample_generated0), .q(new_sample_generated));
-    //dffr #(.WIDTH(16)) pipeline_ff_sample_out (.clk(clk), .r(reset), .d(sample_out0), .q(sample_out));
+    dffr pipeline_ff_nsg (
+        .clk(clk),
+        .r(reset),
+        .d(new_sample_generated0),
+        .q(new_sample_generated)
+    );
+
+    // If you want one more pipeline stage for sample_out, you can re-enable:
+    // dffr #(.WIDTH(16)) pipeline_ff_sample_out (
+    //     .clk(clk),
+    //     .r(reset),
+    //     .d(sample_out0),
+    //     .q(sample_out)
+    // );
+    // For now, pass through directly:
     assign sample_out = sample_out0;
 
     assign new_sample_generated0 = generate_next_sample;
+
     codec_conditioner codec_conditioner(
         .clk(clk),
         .reset(reset),
-        .new_sample_in(note_sample),
+        // FEED ECHO OUTPUT INTO CODEC, NOT RAW NOTE SAMPLE
+        .new_sample_in(echo_out),
         .latch_new_sample_in(note_sample_ready),
         .generate_next_sample(generate_next_sample0),
         .new_frame(new_frame),
